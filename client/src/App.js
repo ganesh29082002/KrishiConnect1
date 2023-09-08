d0ed9b6eab486ecd7740b4290d418096df0565fc
purvadhopade <purvadhopade@gmail.com>
2023-09-08 16:20:40 +0530

    new changes added

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,7 @@ import React from 'react'
 export default function App() {
   return (
     <div>
-      <h1>Hiiii Ganuuu</h1>
-      <h2>fdjufbjifgbhj0iew</h2>
+      <h1>Hello</h1>
     </div>
   )
 }
