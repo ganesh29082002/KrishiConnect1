8da9fa6de7e9f3d9c2aa19bd86a28e2d1c214ac5
ganesh29082002 <83921516+ganesh29082002@users.noreply.github.com>
2023-09-23 18:45:41 +0530

    add navbar of home page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -5,9 +5,12 @@ import AboutUs from '../components/AboutUs';
 import Caraousal from '../components/common/Caraousal'
 import Contact from "../components/Contact";
 import Service from "../components/Platform";
+import NavigationBar from "../components/NavigationBar";
+
 export default function HomePage() {
   return (
     <>
+    <NavigationBar />
        <Caraousal/>
        <Service/>
        <AboutUs/>
