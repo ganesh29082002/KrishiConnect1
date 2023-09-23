336f1d30749d49fb42f46ae62bbb1988330cca23
ganesh29082002 <83921516+ganesh29082002@users.noreply.github.com>
2023-09-23 18:45:20 +0530

    add comman navbar

--- client/src/featurePages/weatherAPI/WeatherIndex.jsx
+++ client/src/featurePages/weatherAPI/WeatherIndex.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 
 
 import Weather from '../../components/weather/Weather';
+import NavigationBar from "../../components/CommonNavigation";
+
 
 
 export default function WeatherIndex() {
   
   return (
+    <>
+    <NavigationBar />
     
     <Weather/>
+    </>
     
   )
 }
