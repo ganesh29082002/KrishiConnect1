8faf310dac1e13d1405573f0ec1f716ca9135596
ganesh29082002 <83921516+ganesh29082002@users.noreply.github.com>
2023-09-26 23:19:58 +0530

    model added in navbar

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -58,18 +58,53 @@ const NavigationBar = () => {
 <div class="modal-dialog " role="document">
   <div class="modal-content">
     <div class="modal-header">
-      <h5 class="modal-title" id="exampleModalLongTitle">Farmer Registration</h5>
+      <h5 class="modal-title darkGreenText" id="exampleModalLongTitle">Farmer Registration</h5>
       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
+        <span className="mediumGreenText" aria-hidden="true">&times;</span>
       </button>
     </div>
-    <div class="modal-body">
-      form
+    <div className="modal-body">
+                <div className="mb-3">
+                  <label className="form-label mediumGreenText">Email address</label>
+                  <input
+                    // onChange={onChangeHandler}
+                    // value={data.email}
+                    type="email"
+                    name="email"
+                    className="form-control shadow-none"
+                  />
+                </div>
+
+                <div className="mb-4">
+                  <label className="form-label mediumGreenText">Password</label>
+                  <input
+                    // onChange={onChangeHandler}
+                    // value={data.password}
+                    type="password"
+                    name="password"
+                    className="form-control shadow-none"
+                  />
+                </div>
+
+                <div className="d-flex align-items-center justify-content-between mb-2">
+                  <button
+                    // onClick={(e) => onLogin(e)}
+                    className="btn darkGreenbg SHADOW-NONE w-100" data-dismiss="modal"
+                  >
+                    Login
+                  </button>
+                 
+                </div>
+              </div>
+              <div class="modal-footer d-flex justify-content-between">
+    <div style={{display : "block"}}>
+        <h6 className="mediumGreenText">Not Registered Yet ? Click on Register</h6>
     </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-     
+    <div>
+        <button type="button" class="btn btn-outline-success w-100" data-dismiss="modal">Register</button>
     </div>
+</div>
+
   </div>
 </div>
 </div>
