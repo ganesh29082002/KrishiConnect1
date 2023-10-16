a7e398f6b987800bde6a9b2ec25c6ebd73417f63
ganesh29082002 <83921516+ganesh29082002@users.noreply.github.com>
2023-10-16 13:37:21 +0530

    backend entry point setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
-console.log("hello world")
+const express  =require("express");
+const router = require("./routes/AuthenicationRoutes")
+const cookieParser = require("cookie-parser");
+const bodyParser=  require("body-parser")
+
+require("dotenv").config();
+require("./config/Database").connect();
+const cors = require("cors");
+const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.json({extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use("/api", router);
+
+
+
+app.listen(process.env.PORT ,() =>{
+   console.log(`server is running on port ${process.env.PORT}`)
+})
