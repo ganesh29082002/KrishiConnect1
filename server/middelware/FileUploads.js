e6c71ff4607a2f27edc01150d303661019662702
ganesh29082002 <83921516+ganesh29082002@users.noreply.github.com>
2023-11-21 13:02:10 +0530

    aws code fix

--- server/middelware/FileUploads.js
+++ server/middelware/FileUploads.js
@@ -14,26 +14,3 @@ const upload = multer({ storage });
 module.exports = upload;
 
 
-// s3Middleware.js
-
-// const AWS = require('aws-sdk');
-// const multer = require('multer');
-// const multerS3 = require('multer-s3');
-
-// const s3 = new AWS.S3({
-//   accessKeyId: 'AKIAXBK3P2OLRB7KK65Y',
-//   secretAccessKey: 'oIfGYD3Cv0kXNpM0hZuvIKlMm5qqNcAFK+mnEOg5',
-// });
-
-// const upload = multer({
-//   storage: multerS3({
-//     s3: s3,
-//     bucket: 'dummybucket.dev',
-//     acl: 'public-read', // Set permissions as needed
-//     key: function (req, file, cb) {
-//       cb(null, file.originalname);
-//     },
-//   }),
-// });
-
-// module.exports = upload;
