2d87b3a8767f2741a466bb75dd4a3b73b3804eb0
ganesh29082002 <83921516+ganesh29082002@users.noreply.github.com>
2023-11-28 14:38:12 +0530

    public file added

--- server/middelware/FileUploads.js
+++ server/middelware/FileUploads.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../client/public/uploads');
+    cb(null, '../client/public');
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname);
