ecc16de79c2dc6b13abaaf7f50d77acca25ae9f8
ganesh29082002 <83921516+ganesh29082002@users.noreply.github.com>
2023-11-28 13:24:06 +0530

    constrain added to  react cummunity support

--- client/src/components/Platform.js
+++ client/src/components/Platform.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { useUserContext } from '../context/UserContext';
 
 import './platform.css'
 import schmelogo from '../schmelogo.png';
@@ -9,6 +10,11 @@ import men from '../men.png';
 import menlogo from '../menlogo.png';
 import logo from '../logo.png';
 function ServiceSection() {
+  const { details ,setDetails } = useUserContext();
+//   const value1=details.data.email;
+//   const value2=details.data.password;
+// const url = `http://localhost:3000/?value1=${value1}&value2=${value2}`;
+const url = `http://localhost:3000/`
   return (
 
     <section  className="text-center">
@@ -39,12 +45,12 @@ function ServiceSection() {
           </div>
           
           <div className="col-lg-4 col-md-6 mb-3 " style={{height:"300px"}}>
-          <Link to="/weather/home" style={{ textDecoration: 'none' }}> 
+          <Link to={url} style={{ textDecoration: 'none' }}> 
             <div className="service cardGrid">
               <img src={logo} alt="" />
-              <h5>Crop Information</h5>
+              <h5>Ecommerce</h5>
               <p className=' lightGreenText'>
-              Crop-specific guidance on cultivation and management.
+              E-commerce delivery for a seamless online shopping experience..
               </p>
             </div>
             </Link>
@@ -72,6 +78,8 @@ function ServiceSection() {
             </Link>
           </div>
           <div className="col-lg-4 col-md-6 mb-3 " style={{height:"300px" }}>
+          {
+          localStorage.getItem("access_token")?(
           <Link to="/community/home" style={{ textDecoration: 'none' }}> 
             <div className="service cardGrid">
               <img src={menlogo} alt="" />
@@ -80,7 +88,15 @@ function ServiceSection() {
               Community support for farmers to connect, share experiences, and seek expert advice.
               </p>
             </div>
-            </Link>
+            </Link>):(<Link to="/" style={{ textDecoration: 'none' }}> 
+            <div className="service cardGrid">
+              <img src={menlogo} alt="" />
+              <h5>Community Support</h5>
+              <p className=' lightGreenText'>
+              Community support for farmers to connect, share experiences, and seek expert advice.
+              </p>
+            </div>
+            </Link>) }
           </div>
           <div className="col-lg-4 col-md-6 mb-3 " style={{height:"300px"}}>
           <Link to="/articals/home" style={{ textDecoration: 'none' }}> 
