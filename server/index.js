1f2d909f0e1c1d507a4811ae311d00dd90cd86e0
ganesh29082002 <83921516+ganesh29082002@users.noreply.github.com>
2023-11-28 13:27:23 +0530

    add community support at backend

--- server/index.js
+++ server/index.js
@@ -3,12 +3,20 @@ const router = require("./routes/AuthenicationRoutes")
 const articalrouter = require("./routes/UploadArticalRoute")
 const utilityRouter = require("./routes/OtherUtility")
 const cookieParser = require("cookie-parser");
-const bodyParser=  require("body-parser")
+const socketIo = require('socket.io');
 
+const bodyParser=  require("body-parser")
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server , {
+    cors:{
+        origin:"*"
+    }
+});
 require("dotenv").config();
 require("./config/Database").connect();
 const cors = require("cors");
-const app = express();
 
 app.use(cookieParser());
 app.use(bodyParser.json({extended: true }));
@@ -19,10 +27,21 @@ app.use("/api", router);
 app.use("/articalapi", articalrouter);
 app.use("/otherutility", utilityRouter);
 
+const messages = []; // Array to store messages
+
+io.on('connection', (socket) => {
+  // Send stored messages to the newly connected client
+  socket.emit('init', messages);
+
+  socket.on('chat message', (message) => {
+    // Store the message and broadcast it to all connected clients
+    messages.push(message);
+    io.emit('chat message', message);
+  });
+});
 
 
 
-
-app.listen(process.env.PORT ,() =>{
+server.listen(process.env.PORT ,() =>{
    console.log(`server is running on port ${process.env.PORT}`)
 })
