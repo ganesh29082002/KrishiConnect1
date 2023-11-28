18f5d865e76b4bc244cdb52db58db09af3e44feb
ganesh29082002 <83921516+ganesh29082002@users.noreply.github.com>
2023-11-28 13:28:49 +0530

    change user model

--- server/model/User.js
+++ server/model/User.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     fullname: { type: String, default: null },
+    name: { type: String , trim : true },
   email: { type: String, unique: true },
   password: { type: String },
-  role: {type: String},
-  phoneno : {type : Number},
-  city : {type : String},
+  roleganesh: {type: Number},
+  role: {type: Number , default :0},
+  phone : {type : String},
+  address : {type : String},
   verified :{type:Boolean , default:false},
   token: { type: String },
   resetToken: String,
   resetTokenExpiration: Date,
-});
+},{timestamps:true});
 
 module.exports = mongoose.model("users", userSchema);
